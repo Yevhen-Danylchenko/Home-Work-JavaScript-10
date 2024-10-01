40c8e429410faa9fec35a9393dc1ce5e2632cb16
Yevhen-Danylchenko <103068879+Yevhen-Danylchenko@users.noreply.github.com>
2024-10-01 23:42:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const link = document.querySelector('#menu');
+const buttonContent = document.querySelector('#burger');
+const divButtonText = document.querySelector('#btn');
+const leng = document.querySelector('#leng-content');
+const divContainer = document.querySelector('#div-container');
+
+buttonContent.addEventListener('click', () => {
+    link.classList.toggle('hidden');
+    divButtonText.classList.toggle('hidden');
+    leng.classList.toggle('hidden');
+    divContainer.classList.toggle('divCont');
+})
+
